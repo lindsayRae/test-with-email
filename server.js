76cd1c6cb9462874ae6e5c47a2cf4c80a6ad9a9c
Lindsay <lbarnett712@gmail.com>
2022-10-12 07:35:45 -0400

    added logic to handle production

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // serve any static files
+  app.use(express.static(path.join(__dirname, 'client')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
