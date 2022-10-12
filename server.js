ebfa5b2de7d1ce2086b40f9da1e50b6f6f836ce5
Lindsay <lbarnett712@gmail.com>
2022-10-12 08:17:09 -0400

    fixed folder name to public

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.get('/api', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // serve any static files
-  app.use(express.static(path.join(__dirname, 'client')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 }
 
